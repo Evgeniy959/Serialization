ddd3fa58c3ad84a0871423e1d5033f86866d2b72
Starinin Andrey <starininandrey@gmail.com>
2021-10-02 14:14:15 +0300

    Update

--- SerializationLib/CsvSerialization.cs
+++ SerializationLib/CsvSerialization.cs
@@ -1,7 +1,46 @@
-﻿namespace SerializationLib
+﻿using System;
+using System.Reflection;
+
+namespace SerializationLib
 {
-    public class CsvSerialization
+    public static class CsvSerialization
     {
-        
+        public static string Serialize(object obj)
+        {
+            if (obj is null) throw new ArgumentNullException();
+
+            var type = obj.GetType();
+            var variables = type.GetFields();
+
+            return FieldToString(variables, obj);
+        }
+
+        private static string FieldToString (FieldInfo[] fields, object obj)
+        {
+            var res = string.Empty;
+            
+            foreach (var variable in fields)
+            {
+                if (variable.FieldType.IsPrimitive)
+                {
+                    res += $"{variable.GetValue(obj)}|";
+                }
+                else
+                {
+                    if (variable.FieldType.IsEnum)
+                    {
+                        res += $"{variable.GetValue(obj)}|";
+                    }
+                    else
+                    {
+                        var variables = variable.FieldType.GetFields();
+                        var temp = variable.GetValue(obj);
+                        res += FieldToString(variables, temp);
+                    }
+                }
+            }
+
+            return res;
+        }
     }
 }
